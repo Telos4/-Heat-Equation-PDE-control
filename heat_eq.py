0a360f291055f26686d506cb4ab8afe8a5f24c76
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2017-11-08 14:18:31 +0100

    Added computation of gradient by adjoint and by finite differences. The adjoint gradient is not working yet. Also general code cleanup.

--- heat_eq.py
+++ heat_eq.py
@@ -1,19 +1,24 @@
 from __future__ import absolute_import, division, print_function
 from firedrake import *
 from os.path import abspath, basename, dirname, join
+import numpy as np
 
 
 class HeatEq(object):
     def __init__(self, mesh):
-        self.verbose = True
+        self.verbose = False
         self.mesh = mesh
-        self.dt = 0.001
-        #self.k = 0.257
-        self.k = 1.0
+        self.dt = 0.1
+        #self.alpha = 0.257
+        self.alpha = 1.0
         self.u = 25.0
         self.gamma_c = 1.0e3
         self.gamma_i = 0.0
         self.S = FunctionSpace(self.mesh, "CG", 1)
+
+        self.y_omega = Function(self.S)
+        self.y_omega.assign(1.0)
+        self.lmda = 1.0e-3
 
         self.heat_eq_solver_parameters = {
             "mat_type": "aij",
@@ -37,7 +42,7 @@ class HeatEq(object):
 
         self.v = TestFunction(self.S)
 
-        self.N = 100
+        self.N = 2
 
         self.y_ol = []
         for i in range(0, self.N + 1):
@@ -57,12 +62,10 @@ class HeatEq(object):
         N = self.N
         h = Constant(self.dt)
         gamma = [self.gamma_i, self.gamma_i, self.gamma_c, self.gamma_i]
-        ac = Constant(self.k)
+        alpha = Constant(self.alpha)
 
         # set initial value
         self.y_ol[0].assign(y0)
-
-        self.outfile_y.write(self.y_ol[0])
 
         y_out = 20.0
 
@@ -70,12 +73,12 @@ class HeatEq(object):
             v = TestFunction(self.S)
             z = [y_out, y_out, us[k], y_out]
 
-            a = (self.y_ol[k+1] * v + h * ac * inner(grad(self.y_ol[k+1]), grad(v))) * dx
+            a = (self.y_ol[k+1] * v + h * alpha * inner(grad(self.y_ol[k+1]), grad(v))) * dx
             for i in range(1, 5):
-                a += h * ac * Constant(gamma[i - 1]) * self.y_ol[k+1] * v * ds(i)
+                a += h * alpha * Constant(gamma[i - 1]) * self.y_ol[k+1] * v * ds(i)
             a -= inner(self.y_ol[k], v) * dx
             for i in range(1, 5):
-                a -= h * ac * Constant(gamma[i - 1]) * Constant(z[i - 1]) * v * ds(i)
+                a -= h * alpha * Constant(gamma[i - 1]) * Constant(z[i - 1]) * v * ds(i)
 
             heat_eq_problem = NonlinearVariationalProblem(a, self.y_ol[k+1])
 
@@ -85,25 +88,26 @@ class HeatEq(object):
 
             heat_eq_solver.solve()
 
-            self.outfile_y.write(self.y_ol[k+1])
+        for k in range(0,N+1):
+            self.outfile_y.write(self.y_ol[k])
 
     def open_loop_solve_adjoint(self, pT):
         # solve the adjoint PDE
         N = self.N
         h = Constant(self.dt)
         gamma = [self.gamma_i, self.gamma_i, self.gamma_c, self.gamma_i]
-        ac = Constant(self.k)
+        alpha = Constant(self.alpha)
 
         # set initial value
         self.p_ol[0].assign(pT)
 
         for k in range(0, N):
-            print("k = %i" % k)
+            #print("k = %i" % k)
             v = TestFunction(self.S)
-            a = ((self.p_ol[k+1] - self.p_ol[k] - h * (self.y_ol[N-k])) * v) * dx
-            a +=  h * ac * inner(grad(self.p_ol[k+1]), grad(v)) * dx
+            a = ((self.p_ol[k+1] - self.p_ol[k] - h * (self.y_ol[N-k] - self.y_omega)) * v) * dx
+            a += h * alpha * inner(grad(self.p_ol[k+1]), grad(v)) * dx
             for i in range(1, 5):
-                a += h * ac * Constant(gamma[i - 1]) * self.p_ol[k+1] * v * ds(i)
+                a += h * alpha * Constant(gamma[i - 1]) * self.p_ol[k+1] * v * ds(i)
 
             heat_eq_adj_problem = NonlinearVariationalProblem(a, self.p_ol[k+1])
 
@@ -118,87 +122,61 @@ class HeatEq(object):
         for k in range(0,N+1):
             self.outfile_p.write(self.p_ol[k])
 
-    def setup_solver(self):
-        v = TestFunction(self.S)
-        self.y1 = Function(self.S)
-        self.y1.rename("temperature")
+    def compute_gradient(self, u_n):
+        r_n = np.array([0.0 for i in range(0,self.N)])
+        for i in range(0, self.N):
+            p = self.p_ol[i].at([0.5, 0.0])
+            #print("p[{}] = {}".format(i,p))
+            #for j in range(0,10):
+            #    print("p[{}] = {}".format(0.1*j,self.p_ol[i].at([0.1*j, 0.0])))
+            r_n[i] = - (self.lmda * u_n[i] + self.gamma_c * self.alpha * p) # use integral here?
 
-        self.y0 = Function(self.S)
+        return r_n
 
-        #self.y1.assign(self.y0)
+    def compute_gradient_fd(self, y0, u_n):
+        # numerical approximation of the gradient using finite differences
+        eps = 1.0e-3
+        grad_f = np.zeros(self.N)
 
-        self.outfile_y.write(self.y1)
+        for i in range(0,self.N):
+            u_minus = np.copy(u_n)
+            u_plus  = np.copy(u_n)
 
-        h = Constant(self.dt)
-        gamma = [self.gamma_i, self.gamma_i, self.gamma_c, self.gamma_i]
-        ac = self.k
-        u = [Constant(0.0), Constant(0.0), self.u, Constant(0.0)]
+            u_minus[i] -= eps
+            u_plus[i]  += eps
 
-        # ENERGY EQUATION
+            self.open_loop_solve(y0, u_minus)
+            J_minus = self.eval_J(u_minus)
 
-        # BACKWAD EULER
+            self.open_loop_solve(y0, u_plus)
+            J_plus = self.eval_J(u_plus)
 
-        self.a = (self.y1 * v + h * ac * inner(grad(self.y1), grad(v))) * dx
-        for i in range(1,5):
-            self.a += h * ac * Constant(gamma[i-1]) * self.y1 * v * ds(i)
-        self.F = inner(self.y0, v) * dx
-        for i in range(1, 5):
-            self.F += h * ac * Constant(gamma[i-1]) * Constant(u[i-1]) * v * ds(i)
+            # central difference quotient
+            grad_f[i] = (J_plus - J_minus)/(2.0*eps)
 
-        self.heat_eq_problem = NonlinearVariationalProblem(self.a - self.F, self.y1)
+        return grad_f
 
 
-        self.heat_eq_solver = NonlinearVariationalSolver(
-            self.heat_eq_problem,
-            solver_parameters=self.heat_eq_solver_parameters)
+    def eval_J(self, u_n):
+        norm_y = 0.0
+        norm_u = 0.0
 
-    def update_solver(self):
-        v = TestFunction(self.S)
-        h = Constant(self.dt)
-        gamma = [self.gamma_i, self.gamma_i, self.gamma_c, self.gamma_i]
-        ac = self.k
-        u = [0.0, 0.0, self.u, 0.0]
+        for i in range(0,self.N):
+            y_temp = norm(self.y_ol[i] - self.y_omega)**2
+            u_temp = u_n[i]**2
 
-        self.a = (inner(self.y1, v) + h * ac * inner(grad(self.y1), grad(v))) * dx
-        for i in range(1, 5):
-            self.a += h * Constant(gamma[i - 1]) * self.y1 * v * ds(i)
-        self.F = inner(self.y0, v) * dx
-        for i in range(1, 5):
-            self.F += h * Constant(gamma[i - 1]) * Constant(u[i - 1]) * v * ds(i)
+            norm_y += y_temp
+            norm_u += u_temp
 
-        self.heat_eq_problem = NonlinearVariationalProblem(self.a - self.F, self.y1)
+        # final value
+        y_temp = norm(self.y_ol[self.N] - self.y_omega) ** 2
+        norm_y += y_temp
 
-        self.heat_eq_solver = NonlinearVariationalSolver(
-            self.heat_eq_problem,
-            solver_parameters=self.heat_eq_solver_parameters)
+        J = 0.5*norm_y + self.lmda*0.5*norm_u
 
-    def get_fs(self):
-        return self.S
+        return J
 
-    def get_mass_matrix(self):
-        s = TestFunction(self.S)
-        t = TrialFunction(self.S)
-        M = inner(t, s) * dx
 
-        return M
-
-    def get_jacobian_matrix(self):
-        return self.heat_eq_problem.J
-
-    def set_bcs(self, T_bcs):
-        self.T_bcs = T_bcs
-
-    def step(self):
-        if self.verbose:
-            print("HeatEq")
-
-        self.heat_eq_solver.solve()
-
-        self.outfile_y.write(self.y1)
-
-        self.y0.assign(self.y1)
-
-        return self.y1
 
 if __name__ == "__main__":
     cwd = abspath(dirname(__file__))
@@ -210,7 +188,7 @@ if __name__ == "__main__":
 
     heateq.dt = 0.005
 
-    S = heateq.get_fs()
+    S = heateq.S
 
     us = []
     ue = [1.0, 2.0, 3.0, 4.0, 5.0, 10.0, 10.0, 10.0, 10.0, 10.0, 25.0, 25.0, 20.0, 20.0, 20.0]
@@ -220,53 +198,35 @@ if __name__ == "__main__":
         else:
             us.append(0.0)
     y0 = Function(S)
+    y0.assign(0.0)
 
-    print("PDE solve:")
-    heateq.open_loop_solve(y0, us)
+    u_n = np.array([0.0 for i in range(0, heateq.N)])
+    u_n[0] = 1.63
+    u_n[1] = 1.12
 
-    pT = Function(S)
-    pT.interpolate(heateq.y_ol[heateq.N])
-    print("Adjoint solve:")
-    heateq.open_loop_solve_adjoint(pT)
+    for i in range(0,300):
+        # 1. solve PDE
+        heateq.open_loop_solve(y0, u_n)
 
+        J_n = heateq.eval_J(u_n)
+        print("J(y,u) = {}".format(J_n))
 
-    pass
+        # 2. solve Adjoint
+        pT = Function(S)
+        pT.interpolate(heateq.y_ol[heateq.N] - heateq.y_omega)
 
-    # bcT = [DirichletBC(S, Constant(300.0), (1,3)),
-    #        DirichletBC(S, Constant(250.0), (2, 4))]
-    #
-    # heateq.setup_solver()
-    # outfile = File(join(data_dir, "../", "results/", "heateq.pvd"))
-    #
-    # step = 0
-    # t = 0.0
-    # t_end = 2.0
-    # num_timesteps = int(t_end / heateq.dt)
-    # output_frequency = 1
-    #
-    # print("Number of timesteps: {}".format(num_timesteps))
-    # print("Output frequency: {}".format(output_frequency))
-    # print("Number of output files: {}".format(int(num_timesteps / output_frequency)))
-    # print("HeatEq DOFs: {}".format(heateq.y0.vector().size()))
-    #
-    # while (t <= t_end):
-    #     t += heateq.dt
-    #
-    #     print("***********************")
-    #     print("Timestep {}".format(t))
-    #
-    #     y1 = heateq.step()
-    #
-    #     print("")
-    #
-    #     #if step % output_frequency == 0:
-    #     #    outfile.write(y1)
-    #
-    #     step += 1
-    #
-    #     if step % 100 == 0:
-    #             heateq.u = -25.0
-    #             #heateq.update_solver()
-    #
-    #     print("step: " + str(step))
-    #     print("u = " + str(heateq.u))
+        heateq.open_loop_solve_adjoint(pT)
+
+        # 3. compute descent direction
+        r_n = heateq.compute_gradient(u_n)
+
+        # 3.1 compute descrent direction using finite differences
+        grad_f = heateq.compute_gradient_fd(y0, u_n)
+
+        print("grad_f = {}".format(grad_f))
+        print("r_n    = {}".format(r_n))
+
+        #u_n = u_n + 0.1 * r_n
+        u_n = u_n - 0.1 * grad_f
+
+        print("u_n" + str(u_n))
